79a791fc86d20c219b922c4661acd01075089e6a
Mark Martene <markmartene@gmail.com>
2014-02-09 21:53:55 -0800

    Added Movement script, Event Manager script, and Coroutine script

---
+++ Game/Assets/Game/Scripts/EventManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	public delegate void ClickAction();
+	public static event ClickAction OnClicked;
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(Screen.width - 105, 5, 100, 30), "Attack"))
+		{
+			if(OnClicked != null)
+				OnClicked();
+		}
+		if (GUI.Button(new Rect(Screen.width - 105, 40, 100, 30), "Defend"))
+		{
+			if(OnClicked != null)
+				OnClicked();
+		}
+		if (GUI.Button(new Rect(Screen.width - 105, 75, 100, 30), "Ability"))
+		{
+			if(OnClicked != null)
+				OnClicked();
+		}
+		if (GUI.Button(new Rect(Screen.width - 105, 110, 100, 30), "Wait"))
+		{
+			if(OnClicked != null)
+				OnClicked();
+		}
+		if (GUI.Button(new Rect(Screen.width - 105, 145, 100, 30), "Summon"))
+		{
+			if(OnClicked != null)
+				OnClicked();
+		}
+	}
+}
