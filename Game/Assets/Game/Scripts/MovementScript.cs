b0afd90b75a63d68dc680772ff0402fd096983b3
Mark Martene <markmartene@gmail.com>
2014-02-12 17:56:49 -0800

    Changed Networking to use Photon Networking

--- Game/Assets/Game/Scripts/MovementScript.cs
+++ Game/Assets/Game/Scripts/MovementScript.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementScript : MonoBehaviour {
+public class MovementScript : Photon.MonoBehaviour {
 	public int speed = 5;
 	public int gravity = 5;
 	private CharacterController cc;
+	private GameObject player;
+	private PhotonView my_photon_view;
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController>();
+		my_photon_view = PhotonView.Get(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (networkView.isMine){
+		if (my_photon_view.isMine){
 			cc.Move(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, -gravity * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime));
 		}
 		else{
