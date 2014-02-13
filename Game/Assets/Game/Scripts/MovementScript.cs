f37887cb9eeb3541a08b130ac938610a706dea69
Mark Martene <markmartene@gmail.com>
2014-02-12 18:48:48 -0800

    Removed Photon Networking Demos, modified networkmanager script

--- Game/Assets/Game/Scripts/MovementScript.cs
+++ Game/Assets/Game/Scripts/MovementScript.cs
@@ -1,16 +1,32 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MovementScript : Photon.MonoBehaviour {
 	public int speed = 5;
 	public int gravity = 5;
 	private CharacterController cc;
-	private GameObject player;
+	//private GameObject other_player;
 	private PhotonView my_photon_view;
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController>();
 		my_photon_view = PhotonView.Get(this);
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream,
+	                           PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			//We own this player: send the others our data
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		}
+		else
+		{
+			//Network player, receive data
+			//other_player.position = (Vector3)stream.ReceiveNext();
+		}
 	}
 	
 	// Update is called once per frame
