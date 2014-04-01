11040e18285f02fe7f65271c85a0028a2e38af34
Christopher Randin <cdrandin@hotmail.com>
2014-03-31 22:30:42 -0700

    Added functionality to get current focused target for GameManager

---
+++ Game/Assets/Game/Scripts/Unit/class_system/BaseClass.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BaseClass : MonoBehaviour
+{
+	private BaseStat[] _base_stat;
+	private Vital[] _vital;
+
+	void Awake()
+	{
+		_base_stat = new BaseStat[Enum.GetValues(typeof(BaseStat)).Length];
+		_vital = new Vital[Enum.GetValues(typeof(Vital)).Length];
+	}
+
+	public void BaseStatsInit()
+	{
+		for(int i=0;i<_base_stat.Length;++i)
+			_base_stat[i] = new BaseStat();
+	}
+
+	public void VitalsInit()
+	{
+		for(int i=0;i<_vital.Length;++i)
+			_vital[i] = new Vital();
+	}
+
+	public void SetUpBaseStats(params byte[] list)
+	{
+		if(list.Length != _base_stat.Length)
+		{
+			Debug.LogError("SetUpBaseStats(): The number of values does not match the number of _base_stat arguments.");
+			return;
+		}
+
+		// Given a number of arguments, plug them into the BaseStat list
+		byte i=0;
+		foreach(BaseStat.StatName stat in Enum.GetValues(typeof(BaseStat.StatName)))
+		{
+			GetBaseStat(stat).Value = list[i];
+			++i;
+		}
+	}
+
+	public void SetUpVital(params byte[] list)
+	{
+		if(list.Length != _vital.Length)
+		{
+			Debug.LogError("SetUpVital(): The number of values does not match the number of _vital arguments.");
+			return;
+		}
+		
+		// Given a number of arguments, plug them into the Vital list
+		byte i=0;
+		foreach(Vital.VitalName v in Enum.GetValues(typeof(Vital.VitalName)))
+		{
+			GetVital(v).Value = list[i];
+			++i;
+		}
+	}
+
+	public BaseStat GetBaseStat(BaseStat.StatName stat)
+	{
+		return _base_stat[(int)stat];
+	}
+
+	public Vital GetVital(Vital.VitalName v)
+	{
+		return _vital[(int)v];
+	}
+
+}
---
+++ Game/Assets/Game/Scripts/Unit/class_system/Vital.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vital
+{
+	private float _max_val;
+	private float _current_val;
+
+	public Vital()
+	{
+		_current_val = 0;
+		_max_val     = 100.0f;
+	}
+
+	public Vital(float max)
+	{
+		_current_val = _max_val = max;
+	}
+
+	public float Value
+	{
+		get 
+		{
+			// Lowest is 0
+			if(_current_val < 0)
+				return 0;
+			// Most is _max_hp
+			else if(_current_val > _max_val)
+				return _max_val;
+			else
+				return _current_val;
+		}
+		
+		set { _current_val = value; }
+	}
+
+	public enum VitalName
+	{
+		Hp = 0,
+		Exhaust
+	}
+}
+
+/*
+public class Vital
+{
+	private byte _max_hp;
+	private byte _hp;
+
+	private float _exhaust_max;
+	private float _exhaust;
+
+	public Vital(byte max_hp, float max_exhaust)
+	{
+		_hp      = _max_hp = max_hp;
+		_exhaust = _exhaust_max = max_exhaust;
+	}
+
+	// Setters and Getters
+	public byte Hp
+	{
+		get 
+		{
+			// Lowest hp is 0
+			if(_hp < 0)
+				return 0;
+			// Most hp is _max_hp
+			else if(_hp > _max_hp)
+				return _max_hp;
+			else
+				return _hp;
+		}
+
+		set { _hp = value;}
+	}
+
+	public float Exhaust
+	{
+		get { return Mathf.Clamp(_exhaust, 0.0f, _exhaust_max); }
+		set { _exhaust = value; }
+	}
+}
+*/
