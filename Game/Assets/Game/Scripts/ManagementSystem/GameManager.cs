20920d3f197dc52ca8f1b706d0e3e47f5df66601
Christopher Randin <cdrandin@hotmail.com>
2014-04-02 03:11:29 -0700

    Dealt with merge conflicts

--- Game/Assets/Game/Scripts/ManagementSystem/GameManager.cs
+++ Game/Assets/Game/Scripts/ManagementSystem/GameManager.cs
@@ -495,84 +495,7 @@ public static class GameManager
 								Debug.LogError (string.Format ("Unknown player tag! >> {0} <<", leader.transform.tag));
 				}
 		}
-	
-<<<<<<< HEAD
-		/// <summary>
-		/// Add unit into GameManager pool. It will distinguish whose turn it is and put them accoringly into a container.
-		/// </summary>
-		/// <param name="unit">Unit.</param>
-		public static void AddUnitToCurrentPlayersContainer (GameObject unit)
-		{
-				unit.transform.parent = _player_container [(int)GetCurrentPlayer ()].transform;
-		}
 
-		private static void StartTimer ()
-		{
-				_base_time = Time.time;
-		}
-
-		/// <summary>
-		/// Gets the current time.
-		/// </summary>
-		/// <returns>The current time.</returns>
-		public static float GetCurrentTime ()
-		{
-				return Time.time - _base_time;
-		}
-
-		// Reset variables that are required to keep track of info during the game
-		/// <summary>
-		/// Resets the state of the game. Typically used for when the game is over or restart.
-		/// </summary>
-		public static void ResetGameState ()
-		{
-				// Reset values that are used for recording players numbers
-				ResetRecordings ();
-
-				// Reset leaders to be alive
-				ResetLeaders ();
-
-				// Player order	
-				GenerateTurnSequence ();
-
-				_winner = Player.NONE;
-		}
-
-		/// <summary>
-		/// Resets the game manager as well as reseting the game state
-		/// </summary>
-		public static void ResetGameManager ()
-		{
-				_game_init = false;
-
-				ResetGameState ();
-		}
-
-		// Blank resource counts
-		private static void ResetRecordings ()
-		{
-				// Reset round number
-				_round_num = 0;
-
-				// Reset 
-				//_timer = 0.0f;
-
-				for (int i=0; i<_resource_count.Length; ++i) {
-						_resource_count [i] = 0;
-						_resource_spent [i] = 0;
-						_resources_obtained [i] = 0;
-						_units_obtained [i] = 0;
-				}
-				StartTimer ();
-		}
-
-		// All leaders are alive
-		private static void ResetLeaders ()
-		{
-				for (int i=0; i<_leader_status.Length; ++i)
-						_leader_status [i] = Status.Clean;
-		}
-=======
 	/// <summary>
 	/// Add unit into GameManager pool. It will distinguish whose turn it is and put them accoringly into a container.
 	/// </summary>
@@ -664,24 +587,23 @@ public static class GameManager
 		for(int i=0;i<_leader_status.Length;++i)
 			_leader_status[i] = Status.Clean;
 	}
->>>>>>> chris
 	
-		// Currently, shuffles player's turn order
-		private static void GenerateTurnSequence ()
-		{// Sloppy way of doing this
-		
-				List<int> tmp = new List<int> ();
-				int i = 0;
-		
-				while (i<total_players) {
-						int t = Random.Range (0, total_players);
-						if (!tmp.Contains (t)) {
-								tmp.Add (t);
-								_player_turn_order [i] = (Player)t;
-								++i;
-						}
-				}
-		}
+	// Currently, shuffles player's turn order
+	private static void GenerateTurnSequence ()
+	{// Sloppy way of doing this
+	
+			List<int> tmp = new List<int> ();
+			int i = 0;
+	
+			while (i<total_players) {
+					int t = Random.Range (0, total_players);
+					if (!tmp.Contains (t)) {
+							tmp.Add (t);
+							_player_turn_order [i] = (Player)t;
+							++i;
+					}
+			}
+	}
 	
 		// Return recap of the current match
 		// Record stats at the end of the game
