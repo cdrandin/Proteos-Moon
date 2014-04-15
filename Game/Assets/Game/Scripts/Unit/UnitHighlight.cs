031c0b4882acb7a33f09ec9667a9410fbd9dbdc4
Ivan Espinosa <ivan.eespinosa23@gmail.com>
2014-04-14 23:11:50 -0700

    Ignoring problem with highlight

--- Game/Assets/Game/Scripts/Unit/UnitHighlight.cs
+++ Game/Assets/Game/Scripts/Unit/UnitHighlight.cs
@@ -15,23 +15,45 @@ public class UnitHighlight : MonoBehaviour {
 	
 	void OnMouseOver() {
 	
-		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+	
+		foreach(Component child in this.transform)
+		{
+			print (child);
+			Renderer child_render = child.GetComponent<Renderer>();
+			if(child_render != null)
+			{
+				child_render.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+				print ("Hovered over");
+			}
+		}
+		/*
 		foreach (var r in renderers)
 		{
 			// Do something with the renderer here...
 			r.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse"); // like disable it for example. 
+			print ("Hovered over");
 		}
-		
+		*/
 		//renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
 	}
 	
 	void OnMouseExit()
 	{
+		foreach(Transform child in this.transform)
+		{
+			Renderer child_render = child.GetComponent<Renderer>();
+            if(child_render != null)
+            {
+						child_render.material.shader = Shader.Find("Diffuse");
+			}
+		}
+		/*
 		Renderer[] renderers = GetComponentsInChildren<Renderer>();
 		foreach (var r in renderers)
 		{
 			renderer.material.shader = Shader.Find("Diffuse"); 
+			print ("Mouse exit");
 		}
-		
+		*/
 	}
 }
