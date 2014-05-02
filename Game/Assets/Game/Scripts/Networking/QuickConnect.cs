1b388e1eadc289b48a8e22eb8b9bddd6a0e492b9
Mark Martene <markmartene@gmail.com>
2014-05-02 12:03:49 -0700

    Fixed Waiting Room quick connect bug

--- Game/Assets/Game/Scripts/Networking/QuickConnect.cs
+++ Game/Assets/Game/Scripts/Networking/QuickConnect.cs
@@ -4,33 +4,39 @@ using System.Collections;
 public class QuickConnect : Photon.MonoBehaviour {
 	private string playerName = "";
 	private string game_version = "1.0";
-	public void Awake()
+	void Awake()
 	{
-		playerName = PlayerPrefs.GetString("playername");
-		if (string.IsNullOrEmpty(playerName))
-		{
-			PhotonNetwork.playerName = "Guest" + Random.Range(1, 9999);
-			playerName = PhotonNetwork.playerName;
+		if (this.enabled){
+			playerName = PlayerPrefs.GetString("playername");
+			if (string.IsNullOrEmpty(playerName))
+			{
+				PhotonNetwork.playerName = "Guest" + Random.Range(1, 9999);
+				playerName = PhotonNetwork.playerName;
+			}
+			PhotonNetwork.playerName = playerName;
+			PhotonNetwork.ConnectUsingSettings(game_version);
 		}
-		PhotonNetwork.playerName = playerName;
-		PhotonNetwork.ConnectUsingSettings(game_version);
 	}
 	void OnJoinedLobby(){
-		if(PhotonNetwork.JoinRandomRoom()){
-			PhotonNetwork.CreateRoom(null);
+		if (this.enabled){
+			if(PhotonNetwork.JoinRandomRoom()){
+				PhotonNetwork.CreateRoom(null);
+			}
 		}
 	}
 
 	void OnJoinedRoom(){
-		ExitGames.Client.Photon.Hashtable player_props = new ExitGames.Client.Photon.Hashtable();
-		player_props.Add("Leader", "Altier_Seita");
-		PhotonNetwork.player.SetCustomProperties(player_props);
-		if (PhotonNetwork.inRoom){
-			PhotonNetwork.LoadLevel(Application.loadedLevel + 1);
-		}
-		else{
-			PhotonNetwork.offlineMode = true;
-			Application.LoadLevel(Application.loadedLevel + 1);
+		if (this.enabled){
+			ExitGames.Client.Photon.Hashtable player_props = new ExitGames.Client.Photon.Hashtable();
+			player_props.Add("Leader", "Altier_Seita");
+			PhotonNetwork.player.SetCustomProperties(player_props);
+			if (PhotonNetwork.inRoom){
+				PhotonNetwork.LoadLevel(Application.loadedLevel + 1);
+			}
+			else{
+				PhotonNetwork.offlineMode = true;
+				Application.LoadLevel(Application.loadedLevel + 1);
+			}
 		}
 	}
 
