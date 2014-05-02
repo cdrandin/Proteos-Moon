e5c304c97549a89306e9ea344a02d7c4b165857d
Mark Martene <markmartene@gmail.com>
2014-05-02 02:25:29 -0700

    Removed InstantiateSceneObjects from Game, only the masterclient can use that

--- Game/Assets/Game/Scripts/ManagementSystem/Game.cs
+++ Game/Assets/Game/Scripts/ManagementSystem/Game.cs
@@ -69,10 +69,7 @@ public class Game : Photon.MonoBehaviour
 		}
 		else 
 		{
-			if (PhotonNetwork.inRoom)
-				game = PhotonNetwork.InstantiateSceneObject(load_game_objects.name, Vector3.zero, Quaternion.identity, 0) as GameObject;
-			else
-				game = Instantiate(load_game_objects, Vector3.zero, Quaternion.identity) as GameObject;
+			game = Instantiate(load_game_objects, Vector3.zero, Quaternion.identity) as GameObject;
 		}
 
 		//this.gui_method += GUI_menu; 
