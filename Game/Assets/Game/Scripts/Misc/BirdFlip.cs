00918d7cdf2fedb322759febdc9f7e10d26a57ab
Christopher Randin <cdrandin@hotmail.com>
2014-05-02 02:29:44 -0700

    Remove debug

--- Game/Assets/Game/Scripts/Misc/BirdFlip.cs
+++ Game/Assets/Game/Scripts/Misc/BirdFlip.cs
@@ -39,7 +39,6 @@ public class BirdFlip : MonoBehaviour
 		{
 			// start flipping
 			bird_body.transform.Rotate(new Vector3(0,0,rotation_speed*Time.fixedDeltaTime));
-			Debug.Log(bird_body.transform.rotation.eulerAngles.z - _start_rot);
 			if(bird_body.transform.rotation.eulerAngles.z - _start_rot <= 0.0)
 			{
 				bird_body.transform.Rotate(0,
