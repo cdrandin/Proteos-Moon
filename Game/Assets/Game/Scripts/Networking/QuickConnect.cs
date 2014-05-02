2b0c39f3c36707c67f65c63ba451093a3bfaaf23
Mark Martene <markmartene@gmail.com>
2014-05-02 01:20:38 -0700

    created a script to speed connection

---
+++ Game/Assets/Game/Scripts/Networking/QuickConnect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickConnect : MonoBehaviour {
+	private string playerName = "";
+	private string game_version = "1.0";
+	public void Awake()
+	{
+		playerName = PlayerPrefs.GetString("playername");
+		if (string.IsNullOrEmpty(playerName))
+		{
+			PhotonNetwork.playerName = "Guest" + Random.Range(1, 9999);
+			playerName = PhotonNetwork.playerName;
+		}
+		PhotonNetwork.playerName = playerName;
+		PhotonNetwork.ConnectUsingSettings(game_version);
+		if(PhotonNetwork.JoinRandomRoom()){
+			PhotonNetwork.CreateRoom(null);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F13)){
+			PhotonNetwork.LoadLevel(0);
+		}
+	}
+}
