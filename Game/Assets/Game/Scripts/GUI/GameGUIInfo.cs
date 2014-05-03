038de4a72340e3af1119e1e982fe51c5e6bba455
Ivan Espinosa <ivan.eespinosa23@gmail.com>
2014-05-03 00:56:04 -0700

    Added Skin to prefab

--- Game/Assets/Game/Scripts/GUI/GameGUIInfo.cs
+++ Game/Assets/Game/Scripts/GUI/GameGUIInfo.cs
@@ -3,10 +3,26 @@ using System.Collections;
 
 public class GameGUIInfo : MonoBehaviour {
 
+	public GUISkin mySkin;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+	
+	void OnGUI(){
+	
+		GUI.skin = mySkin;
+		
+		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+		//GUI.skin.label.fontSize = Screen.height/32;
+		
+		GUI.Label(new Rect(0,0, Screen.width, Screen.height/32 ), string.Format("Is it my turn {0}", GM.instance.IsNextPlayersTurn()));
+		GUI.Label(new Rect(0, Screen.height/32, Screen.width, Screen.height/32 ), string.Format("Player {0} Turn ", (int)GM.instance.CurrentPlayer));
+		
+	}
+	
+	
 	
 	// Update is called once per frame
 	void Update () {
