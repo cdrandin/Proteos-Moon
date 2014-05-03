3577d973d173b7dc6a9c4372cd26f9d07ce64c4e
Christopher Randin <cdrandin@hotmail.com>
2014-05-02 18:23:52 -0700

    Minimap is pretty good. Slight work, but good.

---
+++ Game/Assets/Game/Scripts/Utilities/LeaderSpawnGizmo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaderSpawnGizmo : MonoBehaviour 
+{
+	public float radius;
+
+	void OnDrawGizmosSelected() 
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawSphere(this.transform.position, radius);
+	}
+}
