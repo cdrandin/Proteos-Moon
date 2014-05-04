2cf9e229440d2160b4b1b7d8faa32545d4c4225b
Mark Martene <markmartene@gmail.com>
2014-05-03 22:57:30 -0700

    Renamed TitleEnvironmentBox, organized textures, modified titlegui script

--- Game/Assets/Game/Scripts/Scenes/TitleGUI.cs
+++ Game/Assets/Game/Scripts/Scenes/TitleGUI.cs
@@ -2,38 +2,41 @@
 using System.Collections;
 
 public class TitleGUI : MonoBehaviour {
-	public GUISkin custom_skin = null;
-	private int button_width = 150;
-	private int button_height = 50;
+	public GUISkin skin;
+	private int _button_width = 150;
+	private int _button_height = 50;
 	private float padding;
-	private int half_button_width;
-	private int half_screen_width;
-	private int half_screen_height;
+	private int _half_button_width;
+	private int _half_screen_width;
+	private int _half_screen_height;
 	private int which_button_clicked = 0;
 	private const int BEGIN = 0;
 	private const int OPTIONS = 1;
 	private const int MULTIPLAYER = 2;
 	private bool button_clicked = false;
-	public bool reset_playerprefs = false;
+	public bool resetPlayerName = false;
 	public MonoBehaviour componentToEnable;
 	
 	public Texture2D clicked, highlight;
-	
-	public void Start(){
-		
-		
-		
-		//button_height = Screen.height / 12;
-		padding  = button_height/4;
-		button_width = 64 * button_height / 15;
-		half_button_width = button_width / 2;
-		half_screen_width = Screen.width / 2;
-		half_screen_height = Screen.height / 2;
+
+	private enum Style
+	{
+		header, loading, question, portrait, readybutton, checkmark, leaderinfo, 
+		chatstyle 
+	}
+
+	void Awake(){
+		//_button_height = Screen.height / 12;
+		padding  = _button_height/4;
+		_button_width = 64 * _button_height / 15;
+		_half_button_width = _button_width / 2;
+		_half_screen_width = Screen.width / 2;
+		_half_screen_height = Screen.height / 2;
 		if (this.componentToEnable == null || this.componentToEnable.enabled)
 		{
 			Debug.LogError("To use the Login, the ComponentToEnable should be defined in inspector and disabled initially.");
 		}
-		if(reset_playerprefs)
+		if(resetPlayerName)
 		{
 			PlayerPrefs.SetString("playername", "");
 		}
@@ -42,32 +45,32 @@ public class TitleGUI : MonoBehaviour {
 	
 	public void UpdateSkin(){
 	
-		Texture2D normal = custom_skin.button.normal.background;
+		Texture2D normal = skin.button.normal.background;
 
 		
-		custom_skin.button.hover.background = UnitGUI.CombineTextures(normal, highlight);
-		custom_skin.button.active.background = UnitGUI.CombineTextures(normal, clicked);
+		skin.button.hover.background = UnitGUI.CombineTextures(normal, highlight);
+		skin.button.active.background = UnitGUI.CombineTextures(normal, clicked);
 
-		custom_skin.button.onHover.background = UnitGUI.CombineTextures(normal, highlight);
-		custom_skin.button.onActive.background = UnitGUI.CombineTextures(normal, clicked);
+		skin.button.onHover.background = UnitGUI.CombineTextures(normal, highlight);
+		skin.button.onActive.background = UnitGUI.CombineTextures(normal, clicked);
 		
 	}
 	
 	public void OnGUI(){
-		GUI.skin = custom_skin;
+		GUI.skin = skin;
 		if(!button_clicked){
 			
-			if(GUI.Button(new Rect((Screen.width / 2) - ((64 * button_height / 15)/2), (Screen.height / 2) - 2 * (Screen.height / 12), 64 * button_height / 15, Screen.height / 12), "Begin Story")){
+			if(GUI.Button(new Rect((Screen.width / 2) - ((64 * _button_height / 15)/2), (Screen.height / 2) - 2 * (Screen.height / 12), 64 * _button_height / 15, Screen.height / 12), "Begin Story")){
 				button_clicked = true;
 				which_button_clicked = BEGIN;
 				//Application.LoadLevel("BattleMap");
 			}
-			if(GUI.Button(new Rect((Screen.width / 2) - ((64 * button_height / 15)/2), (Screen.height / 2) - ((Screen.height / 12) - (button_height/4)), 64 * button_height / 15, Screen.height / 12), "Options")){
+			if(GUI.Button(new Rect((Screen.width / 2) - ((64 * _button_height / 15)/2), (Screen.height / 2) - ((Screen.height / 12) - (_button_height/4)), 64 * _button_height / 15, Screen.height / 12), "Options")){
 				button_clicked = true;
 				which_button_clicked = OPTIONS;
 				//Application.LoadLevel("Options");
 			}
-			if(GUI.Button(new Rect((Screen.width / 2) - ((64 * button_height / 15)/2), (Screen.height / 2) + 2*((Screen.height / 12)/4), 64 * button_height / 15, Screen.height / 12), "Multiplayer")){
+			if(GUI.Button(new Rect((Screen.width / 2) - ((64 * _button_height / 15)/2), (Screen.height / 2) + 2*((Screen.height / 12)/4), 64 * _button_height / 15, Screen.height / 12), "Multiplayer")){
 				button_clicked = true;
 				which_button_clicked = MULTIPLAYER;
 				//Application.LoadLevel("Login");
@@ -103,4 +106,17 @@ public class TitleGUI : MonoBehaviour {
 			}
 		}
 	}
+
+	#region Helper Functions
+	
+	private bool MakeButton(float left, float top, string buttonName, Style index){
+		float height = Screen.height / 16;
+		return GUI.Button(new Rect(left, top, 64* height / 15, height), buttonName, skin.customStyles[(int)index]);
+		
+	}
+	private bool MakeButton(Rect box, string buttonName, Style index){
+		return GUI.Button ( box, buttonName, skin.customStyles[(int)index]);
+	}
+
+	#endregion
 }
