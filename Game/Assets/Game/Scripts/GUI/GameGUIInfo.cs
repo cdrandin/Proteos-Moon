174b4c28cd23d974abf1b143172b54d9d310d762
Ivan Espinosa <ivan.eespinosa23@gmail.com>
2014-05-03 20:54:01 -0700

    Update GUI info

--- Game/Assets/Game/Scripts/GUI/GameGUIInfo.cs
+++ Game/Assets/Game/Scripts/GUI/GameGUIInfo.cs
@@ -18,9 +18,8 @@ public class GameGUIInfo : MonoBehaviour {
 
 		GUI.Label(new Rect(0, 0*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("I am {0}", (Player)GM.instance.Photon_Leader.GetPhotonView().owner.ID-1), textColor);
 		GUI.Label(new Rect(0, 1*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("{0}'s Turn ", GM.instance.CurrentPlayer),textColor);
-		GUI.Label(new Rect(0, 2*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("First {0} then {1} Turn ", GM.instance.TurnOrder[0], GM.instance.TurnOrder[1]), textColor);
-		GUI.Label(new Rect(0, 3*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("Is it my turn {0}", GM.instance.IsItMyTurn()), textColor);
-		GUI.Label(new Rect(0, 4*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("IsNextPlayersTurn: {0}", GM.instance.IsNextPlayersTurn()), textColor);
+		GUI.Label(new Rect(0, 2*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("Is it my turn {0}", GM.instance.IsItMyTurn()), textColor);
+		GUI.Label(new Rect(0, 3*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("IsNextPlayersTurn: {0}", GM.instance.IsNextPlayersTurn()), textColor);
 		
 	}
 	
