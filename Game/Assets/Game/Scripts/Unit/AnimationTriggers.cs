35cd5169096fc12b958d3bd067cdeaa5a4b96277
Ivan Espinosa <ivan.eespinosa23@gmail.com>
2014-05-06 01:49:26 -0700

    Added an animation script to test the animations of each of the models that currently have animations. i.e. Braver, Sniper, Seta, Mena, Titan, Scout

---
+++ Game/Assets/Game/Scripts/Unit/AnimationTriggers.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTriggers : MonoBehaviour {
+
+	Animator anim;
+	
+	int attack_hash = Animator.StringToHash("Attack");
+	int damage_hash = Animator.StringToHash("Damage");
+	int death_hash = Animator.StringToHash("Death");
+	int gather_hash = Animator.StringToHash("Gather");
+	int idle_hash = Animator.StringToHash("Idle");
+	int ready_hash = Animator.StringToHash("Ready");
+	
+	
+	int idle_state_hash = Animator.StringToHash("Base Layer.Idle");
+	int ready_state_hash = Animator.StringToHash("Base Layer.Ready");
+	
+	
+	int health = 100;
+	int number_of_attacks;
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+		number_of_attacks = anim.GetInteger("num_of_attacks");
+	}
+	
+	
+	void Update ()
+	{
+		float move = Input.GetAxis ("Vertical");
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		anim.SetFloat("Speed", move);
+		
+		
+		
+		if(Input.GetKeyDown(KeyCode.R) && stateInfo.nameHash == idle_state_hash)
+		{
+			print (gameObject.name + "is Ready");
+			anim.SetTrigger (ready_hash);
+		}
+		else if(Input.GetKeyDown(KeyCode.R) && stateInfo.nameHash == ready_state_hash)
+		{
+
+			print (gameObject.name + "is Idle");
+			anim.SetTrigger (idle_hash);
+		}
+		
+		else if(Input.GetKeyDown(KeyCode.A)){
+			print (gameObject.name + "is Attacking");
+			int attack_value = Random.Range(0, number_of_attacks);
+			anim.SetInteger("attack_style", attack_value);
+			Debug.Log(attack_value);
+			anim.SetTrigger (attack_hash);
+		}
+		
+		else if(Input.GetKeyDown(KeyCode.D)){
+			
+			print (gameObject.name + "is Damaged");
+			health -= 50;
+			anim.SetInteger("Health", health);
+			anim.SetTrigger (damage_hash);
+		}
+		else if(Input.GetKeyDown(KeyCode.G)){
+			
+			print (gameObject.name + "is Gathering");
+			anim.SetTrigger (gather_hash);
+		}
+
+		
+	}
+}
