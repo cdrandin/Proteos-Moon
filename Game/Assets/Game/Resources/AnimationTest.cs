337da963364915735f22cafe922cbcec6a931e51
Ivan Espinosa <ivan.eespinosa23@gmail.com>
2014-05-06 21:50:26 -0700

    Updated Animations and controllers. Added Arcane animations

---
+++ Game/Assets/Game/Resources/AnimationTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour {
+
+	AnimationTriggers anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponentInChildren<AnimationTriggers>()	;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.R)){
+		
+			anim.ReadyAnimation();
+		}
+		else if(Input.GetKeyDown(KeyCode.E)){
+		
+			anim.IdleAnimation();
+		}
+		else if(Input.GetKeyDown(KeyCode.T)){
+			
+			anim.GatherAnimation();
+		}
+		else if(Input.GetKeyDown(KeyCode.Y)){
+			
+			anim.AttackAnimation();
+		}
+		else if(Input.GetKeyDown(KeyCode.U)){
+			
+			anim.DamageAnimation(50);
+		}
+		else if(Input.GetKeyDown(KeyCode.I)){
+			
+			anim.DamageAnimation(0);
+		}
+	}
+}
