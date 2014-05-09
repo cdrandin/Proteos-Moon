c5f02ad27244e702996a4ebe066c55c328b62691
Christopher Randin <cdrandin@hotmail.com>
2014-05-09 15:08:20 -0700

    Fixed current player turn and round number per player

--- Game/Assets/Game/Scripts/GUI/GameGUIInfo.cs
+++ Game/Assets/Game/Scripts/GUI/GameGUIInfo.cs
@@ -19,7 +19,7 @@ public class GameGUIInfo : MonoBehaviour {
 		GUI.Label(new Rect(0, 0*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("I am {0}", (Player)GM.instance.Photon_Leader.GetPhotonView().owner.ID-1), textColor);
 		GUI.Label(new Rect(0, 1*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("{0}'s Turn ", GM.instance.CurrentPlayer),textColor);
 		GUI.Label(new Rect(0, 2*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("Is it my turn {0}", GM.instance.IsItMyTurn()), textColor);
-		GUI.Label(new Rect(0, 3*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("IsNextPlayersTurn: {0}", GM.instance.IsNextPlayersTurn()), textColor);
+		GUI.Label(new Rect(0, 3*Screen.height/32, Screen.width, Screen.height/32 ), string.Format("Current round: {0}", GM.instance.CurrentRound), textColor);
 	}
 	
 	
