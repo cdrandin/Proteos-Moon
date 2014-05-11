1b6bb38553cf37cab5f998fb79d9cde152ec33b7
Mark Martene <markmartene@gmail.com>
2014-05-10 21:39:49 -0700

    Cleaned up TitleGUI script, made a prefab of MainMenu

--- Game/Assets/Game/Scripts/Scenes/TitleGUI.cs
+++ Game/Assets/Game/Scripts/Scenes/TitleGUI.cs
@@ -3,22 +3,20 @@ using System.Collections;
 
 public class TitleGUI : MonoBehaviour {
 	public GUISkin skin;
-	public float _button_width;
-	public float _button_height;
-	//private float padding;
-	private float _half_button_width;
-	private float _half_screen_width;
-	//private float _half_screen_height;
-	private TitleButton which_button_clicked;
-	private bool button_clicked = false;
-	public bool resetPlayerName = false;
-	public MonoBehaviour componentToEnable;
-	
 	public Texture2D clicked, highlight;
+	public MonoBehaviour componentToEnable;
+	public bool resetPlayerName = false;
+	private float _buttonWidth = 180;
+	private float _buttonHeight = 40;
+	private float _halfButtonWidth;
+	private float _halfScreenWidth;
+	private bool _buttonClicked = false;
+	private TitleButton _whichButtonClicked;
+	
+
 	private enum TitleButton {NONE, NEWGAME, CONTINUE, MATCHMAKING, OPTIONS}
 
 	void Awake(){
-		//_button_height = Screen.height / 12;
 		
 		if (this.componentToEnable == null || this.componentToEnable.enabled)
 		{
@@ -29,7 +27,7 @@ public class TitleGUI : MonoBehaviour {
 			PlayerPrefs.SetString("playername", "");
 		}
 		PlayerPrefs.SetInt("state", 0);
-		which_button_clicked = TitleButton.NONE;
+		_whichButtonClicked = TitleButton.NONE;
 		UpdateSkin();
 
 		Ratios();
@@ -38,16 +36,14 @@ public class TitleGUI : MonoBehaviour {
 	
 	private void Ratios(){
 	
-		_button_height = (10 * Screen.height) / 203;
-		//padding  = (135 * Screen.height) / 812;
+		_buttonHeight = (10 * Screen.height) / 203;
 		
-		_button_width = 64 * _button_height / 15;
-		_half_button_width = _button_width / 2;
+		_buttonWidth = 64 * _buttonHeight / 15;
+		_halfButtonWidth = _buttonWidth / 2;
 		
-		_half_screen_width = Screen.width / 2;
-		//_half_screen_height = Screen.height / 2;
+		_halfScreenWidth = Screen.width / 2;
 
-		skin.button.fontSize = (int)_button_height/2;
+		skin.button.fontSize = (int)_buttonHeight/2;
 		
 	}
 	
@@ -61,51 +57,48 @@ public class TitleGUI : MonoBehaviour {
 	
 	public void OnGUI(){
 		GUI.skin = skin;
-		Ratios();
-		if(!button_clicked){
+		if(!_buttonClicked){
 			
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (255*Screen.height / 812), _button_width, _button_height), "New Game")){
-				button_clicked = true;
-				which_button_clicked = TitleButton.NEWGAME;
+			if(GUI.Button(new Rect(_halfScreenWidth - _halfButtonWidth, Screen.height - (255*Screen.height / 812), _buttonWidth, _buttonHeight), "New Game")){
+				_buttonClicked = true;
+				_whichButtonClicked = TitleButton.NEWGAME;
 			}
 			if(PlayerPrefs.GetInt("state") == 0){
 				GUI.enabled = false;
 			}
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (215*Screen.height / 812), _button_width, _button_height), "Continue")){
-				button_clicked = true;
-				which_button_clicked = TitleButton.CONTINUE;
+			if(GUI.Button(new Rect(_halfScreenWidth - _halfButtonWidth, Screen.height - (215*Screen.height / 812), _buttonWidth, _buttonHeight), "Continue")){
+				_buttonClicked = true;
+				_whichButtonClicked = TitleButton.CONTINUE;
 			}
 			if (!GUI.enabled){
 				GUI.enabled = true;
 			}
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (175*Screen.height / 812), _button_width, _button_height), "Matchmaking")){
-				button_clicked = true;
-				which_button_clicked = TitleButton.MATCHMAKING;
+			if(GUI.Button(new Rect(_halfScreenWidth - _halfButtonWidth, Screen.height - (175*Screen.height / 812), _buttonWidth, _buttonHeight), "Matchmaking")){
+				_buttonClicked = true;
+				_whichButtonClicked = TitleButton.MATCHMAKING;
 			}
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (135*Screen.height / 812), _button_width, _button_height), "Options")){
-				button_clicked = true;
-				which_button_clicked = TitleButton.OPTIONS;
+			if(GUI.Button(new Rect(_halfScreenWidth - _halfButtonWidth, Screen.height - (135*Screen.height / 812), _buttonWidth, _buttonHeight), "Options")){
+				_buttonClicked = true;
+				_whichButtonClicked = TitleButton.OPTIONS;
 			}
 		}
 		else
 		{
-			switch(which_button_clicked)
+			switch(_whichButtonClicked)
 			{
 			case TitleButton.NEWGAME:
 				//Application.LoadLevel(Application.loadedLevel + 2);
-				break;
+				//break;
 			case TitleButton.CONTINUE:
 				//Application.LoadLevel(Application.loadedLevel + 2);
-				break;
+				//break;
 			case TitleButton.OPTIONS:
 				//Application.LoadLevel(Application.loadedLevel + 1);
-				break;
+				//break;
 			case TitleButton.MATCHMAKING:
 				if(PlayerPrefs.GetString("playername") == ""){
-					//this.GetComponent
 					this.componentToEnable = this.GetComponent<LoginScript>();
 					this.componentToEnable.enabled = true;
-					//LoginScript.
 				}
 				else
 				{
