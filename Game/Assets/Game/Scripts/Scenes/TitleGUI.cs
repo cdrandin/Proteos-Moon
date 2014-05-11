435bc2fa5568af95cce45381ed53ce17db7cd327
Mark Martene <markmartene@gmail.com>
2014-05-10 21:16:00 -0700

    Changed title buttons to blank buttons, added a continue button

--- Game/Assets/Game/Scripts/Scenes/TitleGUI.cs
+++ Game/Assets/Game/Scripts/Scenes/TitleGUI.cs
@@ -9,16 +9,13 @@ public class TitleGUI : MonoBehaviour {
 	private float _half_button_width;
 	private float _half_screen_width;
 	//private float _half_screen_height;
-	private int which_button_clicked = 0;
-	private const int BEGIN = 0;
-	private const int OPTIONS = 1;
-	private const int MULTIPLAYER = 2;
+	private TitleButton which_button_clicked;
 	private bool button_clicked = false;
 	public bool resetPlayerName = false;
 	public MonoBehaviour componentToEnable;
-	private GUIStyle story, options, multiplayer;
 	
 	public Texture2D clicked, highlight;
+	private enum TitleButton {NONE, NEWGAME, CONTINUE, MATCHMAKING, OPTIONS}
 
 	void Awake(){
 		//_button_height = Screen.height / 12;
@@ -31,12 +28,10 @@ public class TitleGUI : MonoBehaviour {
 		{
 			PlayerPrefs.SetString("playername", "");
 		}
-
+		PlayerPrefs.SetInt("state", 0);
+		which_button_clicked = TitleButton.NONE;
 		UpdateSkin();
-		
-		story = skin.FindStyle("Story");
-		options = skin.FindStyle("Options");
-		multiplayer = skin.FindStyle("Multiplayer");
+
 		Ratios();
 		
 	}
@@ -51,37 +46,16 @@ public class TitleGUI : MonoBehaviour {
 		
 		_half_screen_width = Screen.width / 2;
 		//_half_screen_height = Screen.height / 2;
-		
-		story.fontSize = (int)_button_height/2;
-		options.fontSize = (int)_button_height/2;
-		multiplayer.fontSize = (int)_button_height/2;
+
+		skin.button.fontSize = (int)_button_height/2;
 		
 	}
 	
 	public void UpdateSkin(){
 	
-		Texture2D special = skin.FindStyle("Story").normal.background;
-		Texture2D item = skin.FindStyle("Options").normal.background;
-		Texture2D attack = skin.FindStyle("Multiplayer").normal.background;
-
-		
-		skin.customStyles[0].hover.background = UnitGUI.CombineTextures(special, highlight);
-		skin.customStyles[0].active.background = UnitGUI.CombineTextures(special, clicked);
-
-		skin.customStyles[0].onHover.background = UnitGUI.CombineTextures(special, highlight);
-		skin.customStyles[0].onActive.background = UnitGUI.CombineTextures(special, clicked);
-
-		skin.customStyles[1].hover.background = UnitGUI.CombineTextures(item, highlight);
-		skin.customStyles[1].active.background = UnitGUI.CombineTextures(item, clicked);
-		
-		skin.customStyles[1].onHover.background = UnitGUI.CombineTextures(item, highlight);
-		skin.customStyles[1].onActive.background = UnitGUI.CombineTextures(item, clicked);
-
-		skin.customStyles[2].hover.background = UnitGUI.CombineTextures(attack, highlight);
-		skin.customStyles[2].active.background = UnitGUI.CombineTextures(attack, clicked);
-		
-		skin.customStyles[2].onHover.background = UnitGUI.CombineTextures(attack, highlight);
-		skin.customStyles[2].onActive.background = UnitGUI.CombineTextures(attack, clicked);
+		Texture2D normal = skin.button.normal.background;
+		skin.button.hover.background = UnitGUI.CombineTextures(normal, highlight);
+		skin.button.active.background = UnitGUI.CombineTextures(normal, clicked);
 		
 	}
 	
@@ -90,33 +64,43 @@ public class TitleGUI : MonoBehaviour {
 		Ratios();
 		if(!button_clicked){
 			
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (255*Screen.height / 812), _button_width, _button_height), "Begin Story", story)){
+			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (255*Screen.height / 812), _button_width, _button_height), "New Game")){
 				button_clicked = true;
-				which_button_clicked = BEGIN;
-				//Application.LoadLevel("BattleMap");
+				which_button_clicked = TitleButton.NEWGAME;
 			}
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (215*Screen.height / 812), _button_width, _button_height), "Options", options)){
-				button_clicked = true;
-				which_button_clicked = OPTIONS;
-				//Application.LoadLevel("Options");
+			if(PlayerPrefs.GetInt("state") == 0){
+				GUI.enabled = false;
 			}
-			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (175*Screen.height / 812), _button_width, _button_height), "Multiplayer", multiplayer)){
+			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (215*Screen.height / 812), _button_width, _button_height), "Continue")){
 				button_clicked = true;
-				which_button_clicked = MULTIPLAYER;
-				//Application.LoadLevel("Login");
+				which_button_clicked = TitleButton.CONTINUE;
+			}
+			if (!GUI.enabled){
+				GUI.enabled = true;
+			}
+			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (175*Screen.height / 812), _button_width, _button_height), "Matchmaking")){
+				button_clicked = true;
+				which_button_clicked = TitleButton.MATCHMAKING;
+			}
+			if(GUI.Button(new Rect(_half_screen_width - _half_button_width, Screen.height - (135*Screen.height / 812), _button_width, _button_height), "Options")){
+				button_clicked = true;
+				which_button_clicked = TitleButton.OPTIONS;
 			}
 		}
 		else
 		{
 			switch(which_button_clicked)
 			{
-			case BEGIN:
-				Application.LoadLevel("BattleMap");
+			case TitleButton.NEWGAME:
+				//Application.LoadLevel(Application.loadedLevel + 2);
 				break;
-			case OPTIONS:
-				Application.LoadLevel("Options");
+			case TitleButton.CONTINUE:
+				//Application.LoadLevel(Application.loadedLevel + 2);
 				break;
-			case MULTIPLAYER:
+			case TitleButton.OPTIONS:
+				//Application.LoadLevel(Application.loadedLevel + 1);
+				break;
+			case TitleButton.MATCHMAKING:
 				if(PlayerPrefs.GetString("playername") == ""){
 					//this.GetComponent
 					this.componentToEnable = this.GetComponent<LoginScript>();
