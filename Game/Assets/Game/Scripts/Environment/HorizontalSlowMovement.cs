c3d74f7575d89ebe3d1827a7a06d7e4d1e86e7d9
Christopher Randin <cdrandin@hotmail.com>
2014-05-28 19:22:07 -0700

    Added values to scout prefab. Fixed slow movement

--- Game/Assets/Game/Scripts/Environment/HorizontalSlowMovement.cs
+++ Game/Assets/Game/Scripts/Environment/HorizontalSlowMovement.cs
@@ -4,28 +4,29 @@ using System.Collections;
 [RequireComponent (typeof(BoxCollider))]
 public class HorizontalSlowMovement : MonoBehaviour 
 {
+	private UnitController _uc;
+
 	void Start()
 	{
 		GetComponent<BoxCollider>().isTrigger = true;
+		_uc = GameObject.FindGameObjectWithTag("UnitController").GetComponent<UnitController>();
 	}
 	
 	// Entered the movement impairing object
 	void OnTriggerEnter(Collider other)
 	{
-		BaseClass unit = other.gameObject.GetComponent<BaseClass>();
-		if(unit != null)
+		if(other.tag == "Unit" || other.tag == "Leader")
 		{
-			unit.GetComponent<UnitController>().mod_movement(MOVEMENT_AFFECT.SLOWED);
+			_uc.mod_movement(MOVEMENT_AFFECT.SLOWED);
 		}
 	}
 
 	// Exit the movement impairing object
 	void OnTriggerExit(Collider other)
 	{
-		BaseClass unit = other.gameObject.GetComponent<BaseClass>();
-		if(unit != null)
+		if(other.tag == "Unit" || other.tag == "Leader")
 		{
-			unit.GetComponent<UnitController>().mod_movement(MOVEMENT_AFFECT.NORMAL);
+			_uc.mod_movement(MOVEMENT_AFFECT.NORMAL);
 		}
 	}
 }
