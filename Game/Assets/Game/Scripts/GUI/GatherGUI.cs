0bf1d566aed7075376f7b76ba907b8b92a0d6a79
Christopher Randin <cdrandin@hotmail.com>
2014-05-30 23:29:45 -0700

    Gather GUI fixed position

--- Game/Assets/Game/Scripts/GUI/GatherGUI.cs
+++ Game/Assets/Game/Scripts/GUI/GatherGUI.cs
@@ -8,20 +8,25 @@ public class GatherGUI : MonoBehaviour
 	private Transform _target;
 	private Texture _cur_img;
 	public float scalar;
+	private Vector2 _screen_pos;
+	private bool _once;
 
 	void Start()
 	{
 		scalar = 0.5f;
+		_once = true;
 		_target = null;
 		_cur_img = gather_gui;
 		_gui_on = false;
 	}
+
 	public bool gui_on
 	{
 		set 
 		{
 			_gui_on = value; 
 			_target = GM.instance.CurrentFocus.transform;
+			_once   = true;
 		}
 	}
 	
@@ -36,7 +41,13 @@ public class GatherGUI : MonoBehaviour
 				Vector3 gui_pos = _target.position;
 				gui_pos.y += _target.GetComponent<CharacterController>().height*scalar;
 				Vector2 screen_pos = Camera.main.WorldToScreenPoint(gui_pos);
-				GUI.DrawTexture(new Rect(screen_pos.x + _cur_img.width/2.0f, screen_pos.y + _cur_img.height/2.0f, 30.0f, 30.0f), _cur_img);
+				if(_once)
+				{
+					_screen_pos = screen_pos;
+					_once = false;
+				}
+
+				GUI.DrawTexture(new Rect(_screen_pos.x + _cur_img.width/2.0f, _screen_pos.y + _cur_img.height/2.0f, 30.0f, 30.0f), _cur_img);
 			}
 		}
 	}
